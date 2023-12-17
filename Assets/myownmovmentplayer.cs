33640e0291d6274fa64bb6dc70cfcdda2bc0add5
zehraerdogan07a <151174898+zehraerdogan07a@users.noreply.github.com>
2023-12-17 19:17:28 +0100

    hej
    då

--- Assets/myownmovmentplayer.cs
+++ Assets/myownmovmentplayer.cs
@@ -1,61 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
-public class myownmovmentplayer : MonoBehaviour
+namespace Assets
 {
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingpower = 4;
-    private bool isFacingRight = true;
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-
-
-    // Update is called once per frame
-    void Update()
+    public class myownmovemnts : MonoBehaviour
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        public Animator animator;
+        private float horizontal;
+        private float Speed = 8f;
+        private float jumpingpower = 10f; // Adjusted jumping power
+        private bool isFacingRight = true;
+        private bool isGroundedVar;
+        private Rigidbody2D rb;
+        private float groundCheckRadius = 0.2f;
 
-        if (Input.GetButtonDown("Jump") && isGrounded())
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private LayerMask groundLayer;
+
+        // Update is called once per frame
+        void Update()
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingpower);
+            horizontal = Input.GetAxisRaw("Horizontal") * Speed;
+
+            // Set the "Speed" parameter in the Animator
+            animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+            if (Input.GetButtonDown("Jump") && isGrounded())
+            {
+                // Trigger the "Jump" animation
+                animator.SetTrigger("Jump");
+                rb.velocity = new Vector2(rb.velocity.x, jumpingpower);
+            }
+
+            Flip();
         }
 
-        Flip();
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
-
-    private bool isGrounded()
-    {
-        return rb.velocity.y == 0;
-    }
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        private void FixedUpdate()
         {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingpower);
+            // Use rb.AddForce to apply horizontal movement
+            rb.AddForce(new Vector2(horizontal * Speed, 0f));
+
+            // Limit the horizontal velocity to the Speed
+            float limitedSpeed = Mathf.Clamp(rb.velocity.x, -Speed, Speed);
+            rb.velocity = new Vector2(limitedSpeed, rb.velocity.y);
+
+            // If not moving horizontally, set "Speed" to 0 to transition to idle
+            if (Mathf.Abs(horizontal) < 0.01f)
+            {
+                animator.SetFloat("Speed", 0f);
+            }
         }
 
-        if (Input.GetKeyUp(KeyCode.UpArrow) && rb.velocity.y > 0f)
+        private bool isGrounded()
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 7f);
+            // Use Physics2D.OverlapCircle to check if the player is grounded
+            isGroundedVar = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+            return isGroundedVar;
+        }
+
+        private void Flip()
+        {
+            if ((isFacingRight && horizontal < 0f) || (!isFacingRight && horizontal > 0f))
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+        }
+
+        // This function is called by Animation Events to return to the "Idle" state
+        public void ReturnToIdle()
+        {
+            // Trigger the "Idle" animation
+            animator.SetTrigger("Idle");
         }
     }
 }
